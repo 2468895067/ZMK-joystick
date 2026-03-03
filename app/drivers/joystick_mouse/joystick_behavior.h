97e7d39434eaf8368c9e7976da0e6266c3cf1a0b
2468895067 <69013696+2468895067@users.noreply.github.com>
2026-03-03 11:09:37 +0800

    Delete app directory

--- app/drivers/joystick_mouse/joystick_behavior.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <zmk/behavior.h>
-#include <drivers/joystick_mouse/joystick_mouse.h>
-
-int zmk_behavior_joystick_mouse_always(const zmk_event_t *eh);
-int zmk_behavior_joystick_toggle(const zmk_event_t *eh);
